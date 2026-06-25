025efab01ef812bcfef1a62e7953278bdfed0593
RianDeyvid-35 <rian.bulhoes@gmail.com>
2026-06-25 16:10:33 -0300

    Update conexao.js

--- database/conexao.js
+++ database/conexao.js
@@ -1,18 +1,23 @@
+
 // database/conexao.js
 const { Sequelize } = require('sequelize');
-
-// Importa o driver oficial do Turso
 const libsqlDriver = require('@libsql/sqlite3'); 
 
+// Aqui o código puxa as credenciais que você cadastrou lá no painel do Render
+const tursoUrl = process.env.TURSO_DATABASE_URL;
+const tursoToken = process.env.TURSO_AUTH_TOKEN;
+
 const sequelize = new Sequelize({
-   dialect: 'sqlite', // O Sequelize exige que seja 'sqlite' aqui
-   storage: 'libsql://database-riandeyvid-35.aws-us-east-2.turso.io', // Coloque sua URL e Token do Turso juntos aqui
-   dialectModule: libsqlDriver, // Força o Sequelize a usar o Turso em vez do arquivo local
+   dialect: 'sqlite',
+   // Junta a URL e o Token em uma conexão segura
+   storage: `${tursoUrl}?authToken=${tursoToken}`, 
+   dialectModule: libsqlDriver, 
    logging: false
 });
 
+// Apenas avisa no console do Render se a ponte deu certo
 sequelize.sync() 
-   .then(() => console.log('Conectado ao Turso com sucesso!'))
+   .then(() => console.log('Conectado ao Turso com sucesso! 🎉'))
    .catch(err => console.error('Erro ao conectar ao banco:', err));
 
 module.exports = sequelize;
