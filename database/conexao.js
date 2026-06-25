3028acd59c183d20e39a87b10036a9cce2a27091
RianDeyvid-35 <rian.bulhoes@gmail.com>
2026-06-24 23:19:54 -0300

    atualização 1

--- database/conexao.js
+++ database/conexao.js
@@ -1,10 +1,13 @@
 // database/conexao.js
 const { Sequelize } = require('sequelize');
 const path = require('path');
+// 1. Importa o módulo offline que você instalou
+const sqlite3Offline = require('sqlite3-offline'); 
 
 const sequelize = new Sequelize({
    dialect: 'sqlite',
    storage: path.resolve(__dirname, 'database.db'), // Cria o arquivo na pasta database
+   dialectModule: sqlite3Offline, // <-- 2. ESSA LINHA RESOLVE O ERRO NO RAILWAY!
    logging: false 
 });
 
