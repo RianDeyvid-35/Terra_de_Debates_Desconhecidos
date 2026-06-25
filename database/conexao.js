7d9f1d5ddf5ca5651cab65d4f85b5b416eb5dcf6
RianDeyvid-35 <rian.bulhoes@gmail.com>
2026-06-25 16:20:46 -0300

    Update conexao.js

--- database/conexao.js
+++ database/conexao.js
@@ -5,9 +5,6 @@ const libsqlDriver = require('@libsql/sqlite3');
 const tursoUrl = process.env.TURSO_DATABASE_URL;
 const tursoToken = process.env.TURSO_AUTH_TOKEN;
 
-if (!tursoUrl || !tursoToken) {
-   console.error("❌ ERRO CRÍTICO: As variáveis TURSO_DATABASE_URL ou TURSO_AUTH_TOKEN não foram encontradas no ambiente!");
-}
 
 const sequelize = new Sequelize({
    dialect: 'sqlite',
