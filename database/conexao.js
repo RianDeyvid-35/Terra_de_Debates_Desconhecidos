13a823a62868c0f0da349d558c174777750c968b
RianDeyvid-35 <rian.bulhoes@gmail.com>
2026-06-25 15:35:52 -0300

    atualização do conexão.js

--- database/conexao.js
+++ database/conexao.js
@@ -1,1 +1,9 @@
+// database/conexao.js
+
+// Mantenha os parênteses completamente vazios. 
+// Assim, o Sequelize apenas verifica se o banco existe, sem apagar nenhum dado.
+sequelize.sync() 
+   .then(() => console.log('Conectado ao Turso com sucesso!'))
+   .catch(err => console.error('Erro ao conectar ao banco:', err));
+
 module.exports = sequelize;
